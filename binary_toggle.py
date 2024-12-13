348592eff43351dc9a81877b5390d5c0a6e94b19
Ikromalieva-Sabokhat <ikromalievasabokhat@gmail.com>
2024-12-13 09:37:56 -0800

    versiya 4: 1 ta funksiya qo'shildi.

--- binary_toggle.py
+++ binary_toggle.py
@@ -11,3 +11,15 @@ def binary_toggle(n):
 
 
 print(binary_toggle(5))
+def anagramic_difference(numbers):
+    son = [int(son) for son in str(numbers)]
+    max_son = max(son)
+    min_son = min(son)
+    max_index = son.index(max_son)
+    min_index = son.index(min_son)
+    for i in range(3):
+        if i!=max_index and i != min_index:
+            orta_son = son[i]
+    return int(str(max_son) + str(orta_son) + str(min_son)) - int(str(min_son) + str(orta_son) + str(max_son))
+
+print(anagramic_difference(100))
