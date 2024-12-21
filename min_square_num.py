84a632a20e8bb3067be63a87461440ff07205301
Ikromalieva-Sabokhat <ikromalievasabokhat@gmail.com>
2024-12-20 21:48:14 -0800

    versiya 6: 2 likdan 10 likka o'tkazish funskiyasi

--- min_square_num.py
+++ min_square_num.py
@@ -1,2 +1,9 @@
-text = "python"
-print(text[:])
+def binary_to_decimal(binary_str):
+    decimal = 0
+    power = 0
+    for digit in reversed(binary_str):
+        if digit not in {'0', '1'}:
+            return "Noto'g'ri ikkilik son kiritildi."
+        decimal += int(digit) * (2 ** power)
+        power += 1
+    return decimal
