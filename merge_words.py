b06499089f1a0023311dbba0b40738d51189851d
Ikromalieva-Sabokhat <ikromalievasabokhat@gmail.com>
2024-12-20 21:49:28 -0800

    versiya 7: 10 likdan 2 likka o'tkazish funksiyasi.

--- merge_words.py
+++ merge_words.py
@@ -12,3 +12,12 @@ def merge_words(word1, word2):
     return word1[:len(word1) - len(bir_xil)] + bir_xil + word2[len(bir_xil):]
 
 print(merge_words("python", "online"))
+
+def decimal_to_binary(decimal):
+    if decimal < 0:
+        return "Musbat son kiriting."
+    binary = ""
+    while decimal > 0:
+        binary = str(decimal % 2) + binary
+        decimal //= 2
+    return binary if binary else "0"
