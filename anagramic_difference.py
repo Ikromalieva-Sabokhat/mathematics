78df9bbfba03c7cd50c7f9645e3471f4115d15e2
Ikromalieva-Sabokhat <ikromalievasabokhat@gmail.com>
2024-12-20 21:45:43 -0800

    versiya 1: dastlabki funksiya

--- anagramic_difference.py
+++ anagramic_difference.py
@@ -8,14 +8,3 @@ def anagramic_difference(numbers):
         if i!=max_index and i != min_index:
             orta_son = son[i]
     return int(str(max_son) + str(orta_son) + str(min_son)) - int(str(min_son) + str(orta_son) + str(max_son))
-
-print(anagramic_difference(100))
-def binary_to_decimal(binary_str):
-    decimal = 0
-    power = 0
-    for digit in reversed(binary_str):
-        if digit not in {'0', '1'}:
-            return "Noto'g'ri ikkilik son kiritildi."
-        decimal += int(digit) * (2 ** power)
-        power += 1
-    return decimal
